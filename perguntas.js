2053b80c0ff027e393260d2d0b048ca6cdb6633d
Kauedaniel123 <kauedanielfloriano@gmail.com>
2024-11-21 18:38:45 -0300

    Flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação que permite criar conteúdo dinâmico, controlar mídias, e animar imagens.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscovo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é TypeScript?',
+    'TypeScript é uma linguagem de programação de código aberto da Microsoft que usa tipos para descrever objetos e validar código'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz comida em Inglês?',
+    'Comida em ingles é food'
+)
